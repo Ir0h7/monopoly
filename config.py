ba783e8e7572d3f22827121daa1fce1b32dfa291
vladimirkrivulets <vovakrivulets@yandex.ru>
2024-02-18 19:10:34 +0300

    сделал меню слева при залогиненном человеке

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv(os.path.join(basedir, ".env"))
 
 
 class Config(object):
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "vova")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
